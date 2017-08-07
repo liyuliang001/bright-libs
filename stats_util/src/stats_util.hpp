270f1956ca20b1320e8e1841241843c57bfaf28f
liyuliang001 <liyuliang001@gmail.com>
2017-08-07 01:02:32 -0400

    add GetVar and GetStd to stats_util

--- stats_util/src/stats_util.hpp
+++ stats_util/src/stats_util.hpp
@@ -7,7 +7,7 @@ namespace bright_lib{
 namespace stats_util{
 
 template <class T>
-double GetPercentile(std::vector<T> &v, double pct){
+double GetPercentile(const std::vector<T> &v, double pct){
 	if (v.size() == 0)
 		return 0;
 	double p = pct * (v.size() - 1);
@@ -17,11 +17,28 @@ double GetPercentile(std::vector<T> &v, double pct){
 }
 
 template <class T>
-double GetMean(std::vector<T> &v){
+double GetMean(const std::vector<T> &v){
 	double sum = 0;
-	for (int i = 0; i < v.size(); i++)
+	for (int i = 0; i < (int)v.size(); i++)
 		sum += v[i];
 	return sum / v.size();
+}
+
+template <class T>
+double GetVar(const std::vector<T> &v){
+	double mean = GetMean(v);
+	double sum = 0;
+	for (int i = 0; i < (int)v.size(); i++){
+		double d = v[i] - mean;
+		sum += d * d;
+	}
+	return sum / v.size();
+}
+
+template <class T>
+double GetStd(const std::vector<T> &v){
+	double var = GetVar(v);
+	return sqrt(var);
 }
 
 } /* namespace stats_util */
